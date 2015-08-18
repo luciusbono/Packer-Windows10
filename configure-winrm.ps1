2d6c38e046e262aca423e15b86247f6ecc6fd937
Lucius <lucius@simple.com>
2015-08-17 23:00:01 -0700

    Major cleanup of scripts

--- configure-winrm.ps1
+++ configure-winrm.ps1
@@ -6,16 +6,15 @@ $ifaceinfo = Get-NetConnectionProfile
 Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private 
 
 # Set up WinRM and configure some things
-winrm quickconfig '-transport:http'
+winrm quickconfig -q
 winrm s "winrm/config" '@{MaxTimeoutms="1800000"}'
 winrm s "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
 winrm s "winrm/config/service" '@{AllowUnencrypted="true"}'
-winrm s "winrm/config/client" '@{AllowUnencrypted="true"}'
 winrm s "winrm/config/service/auth" '@{Basic="true"}'
-winrm s "winrm/config/client/auth" '@{Basic="true"}'
-winrm s "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
 
 # Enable the WinRM Firewall rule, which will likely already be enabled due to the 'winrm quickconfig' command above
 Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+
+sc.exe config winrm start= auto
 
 exit 0
